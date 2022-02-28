2ed8a745f5f13cf40bad189d66e4dc015528e4ae
kenebebh <91062943+kenebebh@users.noreply.github.com>
2022-03-01 00:22:59 +0100

    uploaded the initial files

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const toggleButton = document.querySelector(".toggle__button");
-console.log(toggleButton);
 const navbarLinks = document.querySelector(".navbar__links");
-console.log(navbarLinks);
-toggleButton.addEventListener("click", function () {
+
+toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
